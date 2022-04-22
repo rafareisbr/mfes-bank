26a83bd9e8f4111f2c61b9fb930ad834209cac29
Rafael Reis <rafareisbr@gmail.com>
2022-04-22 10:47:14 -0300

    Update

--- bank/api/v2/views.py
+++ bank/api/v2/views.py
@@ -1,7 +1,7 @@
 import decimal
 
 from django.db import transaction
-from rest_framework import viewsets, mixins, generics
+from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.status import HTTP_202_ACCEPTED
